4701a39ba5d2c8c9a29d33fafc157c7e78945128
Ishan <ishanwijes@gmail.com>
2022-06-29 17:50:57 +1000

    created javascript variables, fixed up classname issue, updated border styles

---
+++ assets/index.js
@@ -0,0 +1,15 @@
+// creating variables for HTML elements
+const searchbarInput = document.querySelector('#searchbar-input');
+const searchButton = document.querySelector('#search-button');
+
+const currentCityNameAndDate = document.querySelector('#city-name-and-date');
+const currentCityTemp = document.querySelector('#city-temp');
+const currentCityWind = document.querySelector('#city-wind');
+const currentCityHumidity = document.querySelector('#city-humidity');
+const currentCityUVIndex = document.querySelector('#city-UV-index');
+
+
+
+
+
+
