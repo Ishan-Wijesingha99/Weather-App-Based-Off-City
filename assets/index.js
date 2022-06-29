7d0c40fc7b7427fbe1b67acd2a68cbcffda9aced
Ishan <ishanwijes@gmail.com>
2022-06-29 18:52:11 +1000

    added the fetch api call and displayed name of the city on the UI

--- assets/index.js
+++ assets/index.js
@@ -11,5 +11,21 @@ const currentCityUVIndex = document.querySelector('#city-UV-index');
 
 
 
+// IF YOU CAN'T ADD THE DATE OF THE CURRENT CITY EASILY WITHOUT PAYING ANY MONEY, THEN DON'T WORRY ABOUT IT, IT'S NOT WORTH THE HASSLE.
 
+searchButton.addEventListener('click', function(e) {
+    // prevent reloading of the page
+    e.preventDefault();
+
+    // capture the value that was written in the searchbar
+    let cityName = searchbarInput.value;
+    
+    // based off that value, use fetch request to get the relevant data about the city from the OpenWeather API
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4351b60861d031e4e28b9b53af65fc5d`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        currentCityNameAndDate.textContent = `${data.name}`
+    })
+})
 
